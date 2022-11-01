6bb2adabce89d7845854a05b16ea111767550a02
goncalobraga27 <a97541@alunos.uminho.pt>
2022-11-01 22:58:40 +0000

    Atualizado os sp,ss e cl

--- src/sp.py
+++ src/sp.py
@@ -1,5 +1,7 @@
+from re import T
 import socket
 from sys import argv
+from datetime import datetime
 
 def parsingConfigFile(nameConfig_File):
     file= open (nameConfig_File, "r")        
@@ -21,9 +23,44 @@ def parsingConfigFile(nameConfig_File):
             if(lista_Parametros[1]=='LG'):
                 listaLogFile.append(lista_Parametros[2][:-1])
     return (listaIP_SS,listaPorta_SS,listaLogFile,path_FileDataBase)
+# Exemplo de uma querie recebida : message_id flags 0 0 0 0 domain type
+def processQuery(msg,domain_server):
+    query=str(msg)
+    queryCheck=True
+    lista_ParametrosQuery=query.split(' ')
+    if (int(lista_ParametrosQuery[0])>1 and int(lista_ParametrosQuery[0])<65535):
+        queryCheck=True
+    else:
+        return False
+    lista_Flags=lista_ParametrosQuery[1].split('+')
+    for it in lista_Flags:
+        if (it=='R' or it=='Q' or it=='A'):
+            queryCheck=True
+        else:
+            return False
+    if(lista_ParametrosQuery[2]=='0' and lista_ParametrosQuery[3]=='0' and lista_ParametrosQuery[4]=='0' and lista_ParametrosQuery[5]=='0'):
+            queryCheck=True
+    else:
+            return False
+    if (lista_ParametrosQuery[6]==domain_server):
+        queryCheck=True
+    else:
+        return False
+    if (lista_ParametrosQuery[7]=='DEFAULT' or lista_ParametrosQuery[7]=='SOASP' or lista_ParametrosQuery[7]=='SOAADMIN' or\
+        lista_ParametrosQuery[7]=='SOASERIAL' or  lista_ParametrosQuery[7]=='SOAREFRESH' or  lista_ParametrosQuery[7]=='SOARETRY' or\
+        lista_ParametrosQuery[7]=='SOAEXPIRE' or lista_ParametrosQuery[7]=='NS' or lista_ParametrosQuery[7]=='A' or\
+        lista_ParametrosQuery[7]=='CNAME' or lista_ParametrosQuery[7]=='MX' or lista_ParametrosQuery[7]=='PTR' ):
+                    queryCheck=True
+    else:
+            return False
+    
+    return(queryCheck,int(lista_ParametrosQuery[0]))
+    
+
      
 def main():
     nameConfig_File=argv[1]          # ../Files/ConfigFileSP.txt 
+    domain_server=argv[2]
     listaIP_SS,listaPorta_SS,listaLogFile,path_FileDataBase=parsingConfigFile(nameConfig_File)  
     # O path do ficheiro de dados do SP está armazenado na variável path_FileDataBase
     # A lista com nome listaIP_SS tem armazenado os ips do SS para este SP          Exemplo:  IP-[10.0.1.10,10.0.2.10]
@@ -46,7 +83,17 @@ def main():
     while True:
         msg,add = sck.recvfrom(1024)
         print(msg.decode('utf-8'))
-        print(f"Recebi uma mensagem do cliente {add}")
+        processQuery(msg.decode('utf-8'),domain_server)
+        if (processQuery(msg.decode('utf-8'),domain_server)==False):
+            print("A query pedida não é válida")
+        else:
+            print(f"Recebi uma mensagem do cliente {add}")
+            f=open(listaLogFile[0],"a")  
+            now = datetime.today().isoformat()
+            lineLogFile="{"+str(now)+"} "+"{QR/QE}"+" {"+enderecoSP+":"+str(portaSP)+"} "+ "{"+msg.decode('utf-8')+"}\n"
+            print(lineLogFile)
+            f.write(lineLogFile)
+            f.close()
 
     sck.close()
 
--- src/ss.py
+++ src/ss.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import socket
 from sys import argv
 
@@ -22,8 +23,43 @@ def parsingConfigFile(nameConfig_File):
                 listaLogFile.append(lista_Parametros[2][:-1])
     return (listaIP_SP,listaPorta_SP,listaLogFile,path_FileDataBase)
 
+def processQuery(msg,domain_server):
+    query=str(msg)
+    queryCheck=True
+    lista_ParametrosQuery=query.split(' ')
+    if (int(lista_ParametrosQuery[0])>1 and int(lista_ParametrosQuery[0])<65535):
+        queryCheck=True
+    else:
+        return False
+    lista_Flags=lista_ParametrosQuery[1].split('+')
+    for it in lista_Flags:
+        if (it=='R' or it=='Q' or it=='A'):
+            queryCheck=True
+        else:
+            return False
+    if(lista_ParametrosQuery[2]=='0' and lista_ParametrosQuery[3]=='0' and lista_ParametrosQuery[4]=='0' and lista_ParametrosQuery[5]=='0'):
+            queryCheck=True
+    else:
+            return False
+    if (lista_ParametrosQuery[6]==domain_server):
+        queryCheck=True
+    else:
+        return False
+    if (lista_ParametrosQuery[7]=='DEFAULT' or lista_ParametrosQuery[7]=='SOASP' or lista_ParametrosQuery[7]=='SOAADMIN' or\
+        lista_ParametrosQuery[7]=='SOASERIAL' or  lista_ParametrosQuery[7]=='SOAREFRESH' or  lista_ParametrosQuery[7]=='SOARETRY' or\
+        lista_ParametrosQuery[7]=='SOAEXPIRE' or lista_ParametrosQuery[7]=='NS' or lista_ParametrosQuery[7]=='A' or\
+        lista_ParametrosQuery[7]=='CNAME' or lista_ParametrosQuery[7]=='MX' or lista_ParametrosQuery[7]=='PTR' ):
+                    queryCheck=True
+    else:
+            return False
+    
+    return(queryCheck,int(lista_ParametrosQuery[0]))
+    
+
+
 def main():
     nameConfig_File=argv[1]       #  ../Files/ConfigFileSS.txt 
+    domain=argv[2]
     listaIP_SP,listaPorta_SP,listaLogFile,path_FileDataBase=parsingConfigFile(nameConfig_File)    
     # O path do ficheiro de dados do SS está armazenado na variável path_FileDataBase
     # A lista com nome listaIP_SP tem armazenado o ips do SP para este SS         Exemplo:  IP-[10.0.1.10]
@@ -32,7 +68,6 @@ def main():
     # A lista com nome listaLogFile tem os paths dos logs files do domain e de todos os dominios (all), basicamente é uma lista com prioridades
     # Exemplo : [Files/logfileSS.txt,Files/logfiles.txt]
     #          Log file do dominio do SS, Log file do all
-    
     
     sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -45,7 +80,16 @@ def main():
     while True:
         msg,add = sck.recvfrom(1024)
         print(msg.decode('utf-8'))
-        print(f"Recebi uma mensagem do cliente {add}")
+        if(processQuery(msg.decode('utf-8'),domain)==False):
+            print("A query pedida não é válida")
+        else:
+            print(f"Recebi uma mensagem do cliente {add}")
+            f=open(listaLogFile[0],"a")  
+            now = datetime.today().isoformat()
+            lineLogFile="{"+str(now)+"} "+"{QR/QE}"+" {"+endereco+":"+str(porta)+"} "+ "{"+msg.decode('utf-8')+"}\n"
+            print(lineLogFile)
+            f.write(lineLogFile)
+            f.close()
 
     sck.close()
 
