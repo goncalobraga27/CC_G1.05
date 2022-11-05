a48f0b6507747268c038dfd83ff58a84dc64b675
João Pedro Gonçalves <72882679+jpgoncalves13@users.noreply.github.com>
2022-11-05 23:17:11 +0000

    Update sp.py

--- src/sp.py
+++ src/sp.py
@@ -7,28 +7,6 @@ from parserDataFile import parseDataFile
 from answerQuery import aQuery
 from processQuery import pQuery
 
-def answerQuery(dict_Data_Base,typeValue):
-    # Precimos dos campos response-values(Por exemplo:MX), authorities-values(NS) e extra-values (A para ns e mx)
-    listaRes=[]
-    listaRespValues=dict_Data_Base[typeValue]
-    listaAuthValues=dict_Data_Base["NS"]
-    listaExtraValues=dict_Data_Base["A"]
-    for it1 in listaRespValues:
-        listaP=it1.split(' ')
-        if(listaP[0]=='@'):
-                listaRes.append(it1)
-    for it2 in listaAuthValues:
-        listaP=it2.split(' ')
-        if(listaP[0]=='@'):
-                listaRes.append(it2)
-    for it3 in listaExtraValues:
-        listaP=it3.split(' ')
-        if(typeValue.lower() in listaP[0] or "ns" in listaP[0]):
-                listaRes.append(it3)
-
-    
-
-    return listaRes
 """
 import socket
 
