f0ea557103dd962ba4306976e2e9f1a49d24b642
MiguelSenra <miguelangelo.senra2002@gmail.com>
2023-01-02 18:04:03 +0000

    ajuste

--- src/sdt.py
+++ src/sdt.py
@@ -9,39 +9,40 @@ from messageDNS import MessageDNS
 
 
 class sdt:
-    def __init__(self,ipSDT,nameConfig_File):
-        self.ipSDT=ipSDT
-        self.nameConfig_File=nameConfig_File
-        self.dictionary=dict()
+    def __init__(self, ipSDT, nameConfig_File):
+        self.ipSDT = ipSDT
+        self.nameConfig_File = nameConfig_File
+        self.dictionary = dict()
 
-    def runThreadSDT(ipSDT,dic):
-        lock=threading.Lock()
+    def runThreadSDT(ipSDT, dic):
+        lock = threading.Lock()
         lock.acquire()
-        sck =socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP
-        sck.bind((ipSDT,3333))
+        sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        sck.bind((ipSDT, 3333))
         while True:
-            msg,add = sck.recvfrom(1024)
-            print (dic)
-            m=MessageDNS()
+            msg, add = sck.recvfrom(1024)
+            print(dic)
+            m = MessageDNS()
             msg = m.deserialize(msg)
-            #resposta=dic[msg]
-            #print (resposta)
-            #sck.sendto(resposta.encode('UTF-8'),add)
+            resposta = dic[msg]
+            print(resposta)
+            sck.sendto(resposta.encode('UTF-8'), add)
         lock.release()
-        
+
     def runSDT(self):
         parseConfigFile = parseConfigFileSDT(self.nameConfig_File)
-        dic=parseConfigFile.parsingConfigFile()
-        self.dictionary=dic
-        threading.Thread(target=sdt.runThreadSDT, args=(self.ipSDT,self.dictionary)).start()
-
+        dic = parseConfigFile.parsingConfigFile()
+        self.dictionary = dic
+        threading.Thread(target=sdt.runThreadSDT, args=(
+            self.ipSDT, self.dictionary)).start()
 
 
 def main():
     ipSDT = sys.argv[1]    # Porta:3334
-    nameConfig_File = sys.argv[2]  # ../Files/ConfigFileSDT.txt 
-    stObj = sdt(ipSDT,nameConfig_File)
-    stObj.runSDT()    
+    nameConfig_File = sys.argv[2]  # ../Files/ConfigFileSDT.txt
+    stObj = sdt(ipSDT, nameConfig_File)
+    stObj.runSDT()
+
 
 if __name__ == "__main__":
     main()
